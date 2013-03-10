e9b53be45ebe4c85de538f8f8bb65f643d69c13f
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2013-03-10 15:46:52 -0300

    [2/3] Create the Board::validate method
    This creates the unit tests for the method

--- tests/rules/boardtest.cpp
+++ tests/rules/boardtest.cpp
@@ -24,3 +24,31 @@ void BoardTest::testClear() {
 	}
 }
 
+inline static void testValidateBoard(const std::string values, const bool expected) {
+	Board board;
+	board.readString(values);
+
+	bool actual = board.validate();
+	CPPUNIT_ASSERT_EQUAL(expected, actual);
+}
+
+void BoardTest::testValidateEmptyBoard() {
+	testValidateBoard("000000000|000000000|000000000|000000000|000000000|000000000|000000000|000000000|000000000", true);
+}
+
+void BoardTest::testValidateHalfFullBoard() {
+	testValidateBoard("942005010|000830040|035000000|680900420|700213006|093006075|000000730|020094000|060700954", true);
+}
+
+void BoardTest::testValidateFullBoard() {
+	testValidateBoard("942675318|176832549|835149267|681957423|754213896|293486175|419568732|527394681|368721954", true);
+}
+
+void BoardTest::testValidateHalfFullInvalidBoard() {
+	testValidateBoard("942005010|000830040|035000000|680900420|700213006|903006075|000000730|020094000|060700954", false);
+}
+
+void BoardTest::testValidateFullInvalidBoard() {
+	testValidateBoard("942675318|176832549|835149267|681917423|754253896|293486175|419568732|527394681|368721954", false);
+}
+
--- tests/rules/boardtest.h
+++ tests/rules/boardtest.h
@@ -8,6 +8,12 @@ class BoardTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST_SUITE(BoardTest);
 
 	CPPUNIT_TEST(testClear);
+	
+	CPPUNIT_TEST(testValidateEmptyBoard);
+	CPPUNIT_TEST(testValidateHalfFullBoard);
+	CPPUNIT_TEST(testValidateFullBoard);
+	CPPUNIT_TEST(testValidateHalfFullInvalidBoard);
+	CPPUNIT_TEST(testValidateFullInvalidBoard);
 
 	CPPUNIT_TEST_SUITE_END();
 
@@ -16,5 +22,11 @@ public:
 	void tearDown();
 
 	void testClear();
+
+	void testValidateEmptyBoard();
+	void testValidateHalfFullBoard();
+	void testValidateFullBoard();
+	void testValidateHalfFullInvalidBoard();
+	void testValidateFullInvalidBoard();
 };
 
