6c9c1ec14744939bcddda41496bb300d710416de
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2013-03-23 15:24:36 -0300

    Add the mini-grids to Board::validate

--- src/rules/board.cpp
+++ src/rules/board.cpp
@@ -1,5 +1,7 @@
 #include "board.h"
 
+typedef int BoardValues[9][9];
+
 Bill::Sudoku::Board::Board() {
 	clear();
 }
@@ -56,6 +58,25 @@ bool Bill::Sudoku::Board::isFilled() {
 	return true;
 }
 
+static inline bool validateMiniGrid(BoardValues &values, int x, int y) {
+	int usedValues[10] = {0,};
+
+	for (int i = x; i < (x + 3); i++) {
+		for (int j = y; j < (y + 3); j++) {
+			int v = values[i][j];
+			usedValues[v]++;
+		}
+	}
+
+	for (int i = 1; i < 10; i++)
+	{
+		if (usedValues[i] > 1)
+			return false;
+	}
+
+	return true;
+}
+
 bool Bill::Sudoku::Board::validate() {
 	// First, validate rows and columns
 	for (int i = 0; i < 9; i++) {
@@ -71,6 +92,17 @@ bool Bill::Sudoku::Board::validate() {
 			}
 		}	
 	}
+
+	// Now, validate every mini-grid
+	if (!validateMiniGrid(values, 0, 0)) return false;
+	if (!validateMiniGrid(values, 0, 3)) return false;
+	if (!validateMiniGrid(values, 0, 6)) return false;
+	if (!validateMiniGrid(values, 3, 0)) return false;
+	if (!validateMiniGrid(values, 3, 3)) return false;
+	if (!validateMiniGrid(values, 3, 6)) return false;
+	if (!validateMiniGrid(values, 6, 0)) return false;
+	if (!validateMiniGrid(values, 6, 3)) return false;
+	if (!validateMiniGrid(values, 6, 6)) return false;
 
 	return true;
 }
